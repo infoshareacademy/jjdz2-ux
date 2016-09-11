bf9167f3728c8a509be3912baa5891b44503c5ae
AlliceSpring <allicespring21@gmail.com>
2016-09-11 20:56:23 +0200

    utworzono podklasy RgbToHex, RgbaToHex, WordToHex

--- src/java/com/sionach/ux/color/UnifyColors.java
+++ src/java/com/sionach/ux/color/UnifyColors.java
@@ -4,4 +4,15 @@ package com.sionach.ux.color;
  * Created by ablazejewska on 11.09.16.
  */
 public class UnifyColors {
+
+    public void RgbToHex{  //
+
+    }
+
+    public void RgbaToHex{
+
+    }
+
+    public void WordToHex
+
 }
