7ac850d3d94bc47173189ff6e4a3949a157ef223
drzalek <drzalek83@gmail.com>
2016-09-14 21:54:35 +0200

    metoda zwracajaca liste elementów w klamrach z pliku css

--- src/main/java/com/sionach/csslists/ListsFromCss.java
+++ src/main/java/com/sionach/csslists/ListsFromCss.java
@@ -1,8 +1,28 @@
 package com.sionach.csslists;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by webownia on 14.09.16.
  */
 public class ListsFromCss {
+    private List<String> bracesList = new ArrayList<String>(); //lista do przechowywania elementów w klamrach
+    private List<String> propertiesList = new ArrayList<String>(); //lista z elementami własciwosci pliku css w parach własciwosc: wartosc
+    private List<String> valuesList = new ArrayList<String>();//Lista przechowująca wartosci z pliku css
 
+    public List<String> getBracesList(String cssString) {
+
+        String patternBraces = "(?i)\\{[^}]*\\}"; //wzór do pobrania kodu pomiędzy klamrami {} i umieszczenie w elementach tablicy
+
+        Pattern p = Pattern.compile(patternBraces);
+        Matcher m = p.matcher(cssString);
+        while(m.find()){
+            bracesList.add(m.group().replaceAll("\\{{1}","").replaceAll("\\}{1}",""));
+        }
+
+        return bracesList;
+    }
 }
