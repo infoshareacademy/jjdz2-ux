3095d0e655f2ba8915dd482ceb477a97425385ae
Paweł Orszt <pawel.orszt@gmail.com>
2016-09-15 21:02:20 +0200

    dodanie testów

--- src/main/Test/com/sionach/ux/filemanagment/ReadFilesTest.java
+++ src/main/Test/com/sionach/ux/filemanagment/ReadFilesTest.java
@@ -1,13 +1,30 @@
 package com.sionach.ux.filemanagment;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ReadFilesTest {
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
     @Test
-    public void readFileToString() throws Exception {
-        
+    public void objectNotNull() throws Exception {
+        ReadFiles readFiles = new ReadFiles();
+        assertNotNull(readFiles);
+    }
+    @Test
+    public void openFile() throws Exception {
+        ReadFiles readFiles = new ReadFiles();
+        File tempFile = testFolder.newFile("file.txt");
+        readFiles.readFileToString(tempFile.toString());
     }
 
 }
