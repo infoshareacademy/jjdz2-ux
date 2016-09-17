8b590664de90b750d69a977b16962ee0c9b55ff6
Alice <allicespring21@gmail.com>
2016-09-17 11:25:28 +0200

    plik z tabela name-hex

--- src/java/com/sionach/ux/color/namesHexTable.java
+++ src/java/com/sionach/ux/color/namesHexTable.java
@@ -1,5 +1,7 @@
 package com.sionach.ux.color;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by Alice on 14.09.2016.
  */
@@ -7,13 +9,43 @@ public class namesHexTable {  //klasa zawiera nazwy kolorow i przypisane im HEX
     private String colorName;
     private String colorHex;
 
-    public namesHexTable(String colorName, String colorHex){
+    namesHexTable(String colorName, String colorHex){
+        System.out.println("kontruktor z dwoma parametrami klasy namesHexTable");
         this.colorName = colorName;
         this.colorHex = colorHex;
     }
 
-    public String getColorName() {return colorName};
+    public void setColorName(String colorName){
+        this.colorName = colorName;
+    }
+    public void setColorHex(String colorHex){
+        this.colorHex = colorHex;
+    }
 
-    public String getColorHex() {return colorHex};
+    public String getColorName() {
+        return colorName;
+    }
+
+    public String getColorHex() {
+        return colorHex;
+    }
+
+    public void method1(String line){ //metoda przyjmujaca linie zlozona z dwoch wyrazow
+        // i zapisujaca pierwszy wyraz do colorName, zas drugi do color Hex
+
+        String patternString = " " ;
+        Pattern pattern = Pattern.compile(patternString);
+
+        String[] split = pattern.split(line);
+        if (split.length == 2) {
+            this.colorName = split[0];
+            this.colorHex = split[1];
+        }
+        else {
+            System.out.println("Wrong input line");
+        }
+
+    }
 }
 // complete
+
---
+++ src/java/com/sionach/ux/color/readFileByLines.java
@@ -0,0 +1,7 @@
+package com.sionach.ux.color;
+
+/**
+ * Created by Alice on 17.09.2016.
+ */
+public class readFileByLines {
+}
