2cfad29788e893f88db517683c5de6f6467df5cc
Alice <allicespring21@gmail.com>
2016-09-17 11:54:40 +0200

    plik z tabela name-hex

--- src/java/com/sionach/ux/color/readFileByLines.java
+++ src/java/com/sionach/ux/color/readFileByLines.java
@@ -1,7 +1,27 @@
 package com.sionach.ux.color;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Alice on 17.09.2016.
  */
 public class readFileByLines {
+    public List<String> readFileToList(String filename) throws IOException {
+        List<String> lines = new ArrayList<String>();
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
 }
