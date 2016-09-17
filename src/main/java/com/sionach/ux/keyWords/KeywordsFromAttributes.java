4dca7de4a1089d9066a90212d7a1b4de2517fe8f
drzalek <drzalek83@gmail.com>
2016-09-17 10:01:38 +0200

    Nowa klasa wyszukujaca keywords w atrbutach alt i title

---
+++ src/main/java/com/sionach/ux/keyWords/KeywordsFromAttributes.java
@@ -0,0 +1,29 @@
+package com.sionach.ux.keyWords;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by webownia on 17.09.16.
+ */
+public class KeywordsFromAttributes {
+    private List<String> attributesKeywords = new ArrayList<String>();
+
+    public List<String> getAttributesKeywords(String htmlCode){
+
+        String pattern = "(?i)(title=\"[^\"]*\")|(alt=\"[^\"]*\")"; //wyciagnie wszystkich atryutow alt i title z tagow html
+
+        Pattern p  = Pattern.compile(pattern);
+        Matcher m =  p.matcher(htmlCode);
+
+        while(m.find()){
+
+            attributesKeywords.add(m.group().replaceAll("(?i)(title=\")|(alt=\")","").replaceAll("\"",""));
+        }
+
+        return attributesKeywords;
+
+    }
+}
