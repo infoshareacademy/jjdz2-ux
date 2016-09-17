f2916df55833fe3eeedd362d5f619e97d53736b4
Alice <allicespring21@gmail.com>
2016-09-17 12:22:56 +0200

    plik z tabela name-hex

--- src/main/java/com/sionach/ux/color/namesHexTable.java
+++ src/main/java/com/sionach/ux/color/namesHexTable.java
@@ -3,22 +3,22 @@ package com.sionach.ux.color;
 import java.util.regex.Pattern;
 
 /**
- * Created by Alice on 14.09.2016.
+ * Created by Alice on 17.09.2016.
  */
-public class namesHexTable {  //klasa zawiera nazwy kolorow i przypisane im HEX
+public class namesHexTable {
     private String colorName;
     private String colorHex;
 
     namesHexTable(String colorName, String colorHex){
-        System.out.println("kontruktor z dwoma parametrami klasy namesHexTable");
-        this.colorName = colorName;
-        this.colorHex = colorHex;
+        this.setColorName(colorName);
+        this.setColorHex(colorHex);
     }
 
-    public void setColorName(String colorName){
+    public void setColorName(String colorName) {
         this.colorName = colorName;
     }
-    public void setColorHex(String colorHex){
+
+    public void setColorHex(String colorHex) {
         this.colorHex = colorHex;
     }
 
@@ -30,22 +30,17 @@ public class namesHexTable {  //klasa zawiera nazwy kolorow i przypisane im HEX
         return colorHex;
     }
 
-    public void method1(String line){ //metoda przyjmujaca linie zlozona z dwoch wyrazow
-        // i zapisujaca pierwszy wyraz do colorName, zas drugi do color Hex
-
-        String patternString = " " ;
+    public void method1(String line){
+        String patternString = " ";
         Pattern pattern = Pattern.compile(patternString);
-
         String[] split = pattern.split(line);
-        if (split.length == 2) {
+        if (split.length == 2){
             this.colorName = split[0];
             this.colorHex = split[1];
         }
         else {
-            System.out.println("Wrong input line");
+            System.out.println("Wrong line input");
         }
 
     }
 }
-// complete
-
