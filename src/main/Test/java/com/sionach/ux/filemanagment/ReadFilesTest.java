13db8be3b79d311cd068063ecb341d66a7ecd093
Paweł Orszt <pawel.orszt@gmail.com>
2016-09-17 20:03:52 +0200

    dodanie domyślnej ścieżki dla plików

--- src/main/Test/java/com/sionach/ux/filemanagment/ReadFilesTest.java
+++ src/main/Test/java/com/sionach/ux/filemanagment/ReadFilesTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -20,11 +18,11 @@ public class ReadFilesTest {
         ReadFiles readFiles = new ReadFiles();
         assertNotNull(readFiles);
     }
+
     @Test
     public void openFile() throws Exception {
         ReadFiles readFiles = new ReadFiles();
         File tempFile = testFolder.newFile("file.txt");
         readFiles.readFileToString(tempFile.toString());
     }
-
 }
