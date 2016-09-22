7898952b4458f6a0a4779da1ffee7bd0f323e308
Paweł Orszt <pawel.orszt@gmail.com>
2016-09-22 20:59:07 +0200

    dokoczenie funkcji

--- src/main/java/com/sionach/ux/keyWords/KeywordsFromBolded.java
+++ src/main/java/com/sionach/ux/keyWords/KeywordsFromBolded.java
@@ -1,8 +1,6 @@
 package com.sionach.ux.keyWords;
 
 
-import com.sionach.ux.filemanagment.ReadFiles;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -10,27 +8,18 @@ import java.util.regex.Pattern;
 
 public class KeywordsFromBolded {
 
-    public List<String> boldedKeywords(String htmlCode){
+    public List<String> boldedKeywords(String htmlCode) {
 
-        String pattern = "<b[^>]*>[^</]*</b|<strong[^>]*>[^</]*</strong";
-        Pattern p  = Pattern.compile(pattern);
-        Matcher m =  p.matcher(htmlCode);
+        String pattern = "<b [^>]*>[^</]*</b|<strong[^>]*>[^</]*</strong";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(htmlCode);
 
         List<String> keywords = new ArrayList<>();
 
-        while(m.find()){
-            keywords.add(m.group().replaceAll("<b[^>]*>|<strong[^>]*>", "").replaceAll("</b|</strong", "").replaceAll("[^A-Za-z0-9]", " "));
-
+        while (m.find()) {
+            keywords.add(m.group().replaceAll("<b[^>]*>|<strong[^>]*>", "").replaceAll("</b|</strong", ""));
         }
 
         return keywords;
-    }
-
-    public static void main(String[] args) {
-
-        KeywordsFromBolded keywordsFromBolded = new KeywordsFromBolded();
-        ReadFiles readFiles = new ReadFiles();
-
-        System.out.println(keywordsFromBolded.boldedKeywords(readFiles.readFileToString("testowy.html")));
     }
 }
