01cc81b34d4b824a1eee6b5238613293a424839a
drzalek <drzalek83@gmail.com>
2016-09-29 20:31:50 +0200

    Zmiana nazewnictwa w klasie usuniecie psvm

--- src/main/java/com/sionach/ux/keyWords/KeywordsFromHeadlines.java
+++ src/main/java/com/sionach/ux/keyWords/KeywordsFromHeadlines.java
@@ -10,8 +10,8 @@ import java.util.regex.Pattern;
  */
 public class KeywordsFromHeadlines {
 
-    public List<String> HeadlineKeywords(String htmlCode) {
-        List<String> headlineKeywords = new ArrayList<String>();
+    public List<String> headlineKeywords(String htmlCode) {
+        List<String> headlineKeywordsList = new ArrayList<String>();
         String patternHeadline = "<h[1,2,3]{1}[^>]*>[^</]*</h"; //wzór do wyszukiwania headlinów h1,h2,h3
         String splitSentence = " ";
         Pattern p = Pattern.compile(patternHeadline);
@@ -21,20 +21,11 @@ public class KeywordsFromHeadlines {
             String results = m.group().replaceAll("<h[1,2,3]{1}[^>]*>","").replaceAll("</h","");
             String[] splitWords = results.split(splitSentence);
             if(splitWords.length<=8){
-                headlineKeywords.add(results);
+                headlineKeywordsList.add(results);
             }
         }
 
-        return headlineKeywords;
+        return headlineKeywordsList;
     }
 
-    public static void main(String[] args) {
-        String html = "";
-        KeywordsFromHeadlines keywordsResult = new KeywordsFromHeadlines();
-        List<String> test = keywordsResult.HeadlineKeywords(html);
-
-        for(String item : test){
-            System.out.println(item);
-        }
-    }
 }
