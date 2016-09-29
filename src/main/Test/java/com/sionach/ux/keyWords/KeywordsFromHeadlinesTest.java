2de373fad09d7392a6e5c634a2b08c0c7e1b23c4
drzalek <drzalek83@gmail.com>
2016-09-29 20:33:14 +0200

    inicjalizacja klasy testowaj dla KeywordsFromHeadlines

---
+++ src/main/Test/java/com/sionach/ux/keyWords/KeywordsFromHeadlinesTest.java
@@ -0,0 +1,16 @@
+package com.sionach.ux.keyWords;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by webownia on 29.09.16.
+ */
+public class KeywordsFromHeadlinesTest {
+    @Test
+    public void headlineKeywords() throws Exception {
+
+    }
+
+}
