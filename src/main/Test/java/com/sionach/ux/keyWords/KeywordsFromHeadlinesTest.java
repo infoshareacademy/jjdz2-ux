fd211836e3222dbe16d638eac1c0cc9e565e2f2e
drzalek <drzalek83@gmail.com>
2016-09-29 20:38:15 +0200

    dodanie testu sprawdzajacego slowa kluczowe z tagów h1-h3

--- src/main/Test/java/com/sionach/ux/keyWords/KeywordsFromHeadlinesTest.java
+++ src/main/Test/java/com/sionach/ux/keyWords/KeywordsFromHeadlinesTest.java
@@ -2,6 +2,9 @@ package com.sionach.ux.keyWords;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,6 +13,15 @@ import static org.junit.Assert.*;
 public class KeywordsFromHeadlinesTest {
     @Test
     public void headlineKeywords() throws Exception {
+        String htmlCodeTest = "<h1>Jakis tytul</h1><p>Jakis tekst</p><h2>Jakis tytul kolejny</h2><p>blok tekstu</p><h3>Jakis kolejny tytul ale na wiecej niz osiem wyrazow</h3>";
+        KeywordsFromHeadlines keywords = new KeywordsFromHeadlines();
+        List<String> keywordsList = keywords.headlineKeywords(htmlCodeTest);
+
+        List<String> listTest = new ArrayList<>();
+        listTest.add("Jakis tytul");
+        listTest.add("Jakis tytul kolejny");
+
+        assertEquals(keywordsList, listTest);
 
     }
 
