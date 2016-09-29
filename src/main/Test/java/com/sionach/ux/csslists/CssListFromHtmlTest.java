6951d5978e07405dd8a877740dd07a2a3da0d541
drzalek <drzalek83@gmail.com>
2016-09-29 19:23:02 +0200

    utworzenie pliku do testowania pobierania stylu z pliku html

---
+++ src/main/Test/java/com/sionach/ux/csslists/CssListFromHtmlTest.java
@@ -0,0 +1,21 @@
+package com.sionach.ux.csslists;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by webownia on 29.09.16.
+ */
+public class CssListFromHtmlTest {
+    @Test
+    public void codeInlineList() throws Exception {
+
+    }
+
+    @Test
+    public void codeHeadList() throws Exception {
+
+    }
+
+}
