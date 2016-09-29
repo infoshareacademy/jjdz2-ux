0c0bbf5f5b4d824cf061d8875fde61dd8da39613
drzalek <drzalek83@gmail.com>
2016-09-29 21:45:39 +0200

    utworzenie wzorca do wyszukiwania linkow

--- src/main/java/com/sionach/ux/keyWords/KeywordsFromAnchor.java
+++ src/main/java/com/sionach/ux/keyWords/KeywordsFromAnchor.java
@@ -1,8 +1,15 @@
 package com.sionach.ux.keyWords;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by webownia on 29.09.16.
  */
 public class KeywordsFromAnchor {
-
+    public List<String> keywordsAnchor(String htmlCode){
+        List<String> anchors = new ArrayList<>();
+        String patternLink = "(?i)<a[^>]+>[^</a]+</a>";
+        return anchors;
+    }
 }
