74be0f76194f4836f4bf628aba034559460ac586
drzalek <drzalek83@gmail.com>
2016-10-04 22:16:21 +0200

    Dodanie testu do klasy z anchorami

---
+++ src/test/java/com/sionach/ux/keyWords/KeywordsFromAnchorTest.java
@@ -0,0 +1,27 @@
+package com.sionach.ux.keyWords;
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.*;
+
+/**
+ * Created by webownia on 04.10.16.
+ */
+public class KeywordsFromAnchorTest {
+    @Test
+    public void testKeywordsAnchor() throws Exception {
+        String htmlCodeTest = "<article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum purus quam, vel sollicitudin ipsum malesuada at. Praesent tempus finibus faucibus. Nulla dignissim odio ac mi molestie, ut venenatis massa vestibulum. In <a href=\"http://google.pl\">ultricies est non faucibus</a> dignissim. <a href=\"/\">Donec interdum</a> elementum odio, interdum ullamcorper sem dictum eu. Maecenas tortor odio, fringilla ut euismod sed, sagittis et arcu. Integer aliquam finibus lectus, vitae vulputate magna. <a href=\"/\">Integer at erat dapibus, pulvinar neque eget, suscipit velit</a>. Ut ultricies, odio dapibus laoreet condimentum, ipsum metus ullamcorper ex, sit amet consectetur lectus neque ac libero. </article>";
+        KeywordsFromAnchor anchors = new KeywordsFromAnchor();
+        List<String> keywords = anchors.keywordsAnchor(htmlCodeTest);
+
+        List<String> keywordsTest = new ArrayList<>();
+        keywordsTest.add("ultricies est non faucibus");
+        keywordsTest.add("Donec interdum");
+        assertEquals(keywords, keywordsTest);
+    }
+
+}
