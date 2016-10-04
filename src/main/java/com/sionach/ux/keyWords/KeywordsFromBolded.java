57348fd366aa703f26fe60598f080262196650e4
Paweł Orszt <pawel.orszt@gmail.com>
2016-10-04 18:41:41 +0200

    użycie biblioteki w funkcji oraz poprawa testów

--- src/main/java/com/sionach/ux/keyWords/KeywordsFromBolded.java
+++ src/main/java/com/sionach/ux/keyWords/KeywordsFromBolded.java
@@ -1,45 +1,25 @@
 package com.sionach.ux.keyWords;
 
-
-import com.sionach.ux.filemanagment.ReadFiles;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class KeywordsFromBolded {
 
     public List<String> boldedKeywords(String htmlCode) {
 
-        String pattern = "(?i)<b [^>]*>[^</]*</b>|<strong[^>]*>[^</]*</strong";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(htmlCode);
+        Document doc = Jsoup.parse(htmlCode);
+        Elements elements = doc.select("strong, b");
+        List<String> bolded = new ArrayList<>();
 
-        List<String> keywords = new ArrayList<>();
-
-        while (m.find()) {
-            keywords.add(m.group().replaceAll("<b[^>]*>|<strong[^>]*>", "").replaceAll("</b|</strong", ""));
+        for (Element element : elements) {
+            bolded.add(element.text());
         }
 
-        return keywords;
-
-
-    }
-
-    public static void main(String[] args) {
-
-        KeywordsFromBolded keywordsFromBolded= new KeywordsFromBolded();
-        ReadFiles file = new ReadFiles();
-        Document doc = Jsoup.parse(file.readFileToString("testowy2.html"));
-//        Elements bolded = doc.getElementsByTag();
-        Elements bolded = doc.select("strong, b");
-        String test = bolded.text();
-        System.out.println(test);
-
-        System.out.println(keywordsFromBolded.boldedKeywords(file.readFileToString("testowy2.html")));
+        return bolded;
     }
 }
