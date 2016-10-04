be8641de8ce777a1ca0ced94d81376e87242bda8
Paweł Orszt <pawel.orszt@gmail.com>
2016-10-04 21:01:13 +0200

    dodanie funkcjonalności

--- src/main/java/com/sionach/ux/routing/GetLinks.java
+++ src/main/java/com/sionach/ux/routing/GetLinks.java
@@ -1,7 +1,25 @@
 package com.sionach.ux.routing;
 
-/**
- * Created by morhen on 04.10.16.
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GetLinks {
+
+    public List<String> getlinks(String htmlCode){
+
+        Document doc = Jsoup.parse(htmlCode);
+        Elements elements = doc.select("a[href]");
+        List<String> links = new ArrayList<>();
+
+        for(Element element : elements){
+            links.add(element.attr("href"));
+        }
+
+        return links;
+    }
 }
