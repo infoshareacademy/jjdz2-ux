ab630fc9d4e69231084c6866a56e8a0b855bc673
ablazejewska <allicespring@gmail.com>
2016-10-07 21:54:12 +0200

    dodane testowanie  wyszukiwania rgba i rgb

--- src/test/java/com/sionach/ux/color/ExtractColorsFromDataTest.java
+++ src/test/java/com/sionach/ux/color/ExtractColorsFromDataTest.java
@@ -17,9 +17,15 @@ public class ExtractColorsFromDataTest {
         testStringList.add("background-color: #23d4fa;");
         testStringList.add("background-color: #2312aa;");
         testStringList.add("background-color: #b3d1fa;");
+        testStringList.add("background-color: rgba(255,255,255);");
+        testStringList.add("background-color: rgba(255,232,123);");
+        testStringList.add("background-color: rgba(234,212,134);");
+        testStringList.add("background-color: rgb(1,255,255);");
+        testStringList.add("background-color: rgb(12,232,123);");
+        testStringList.add("background-color: rgb(123,212,134);");
 
         List<String> resultStringList = new ArrayList<String>();
-        resultStringList = ExtractColorsFromData.extractColors(testStringList);
+        resultStringList = ExtractColorsFromData.extractHexRgbRgbaColors(testStringList);
         System.out.println(resultStringList);
 
         assertNotNull(resultStringList);
