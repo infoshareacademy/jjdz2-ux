e9c9d8bf889acc3bfec289ee64ed00ec2a0e6285
ablazejewska <allicespring@gmail.com>
2016-10-07 22:43:13 +0200

    Dodane wyszukiwanie koloru po jego imieniu

--- src/main/java/com/sionach/ux/color/ExtractColorsFromData.java
+++ src/main/java/com/sionach/ux/color/ExtractColorsFromData.java
@@ -21,6 +21,27 @@ public class ExtractColorsFromData {
                 extractColorsList.add(results);
             }
         }
+
+        return extractColorsList;
+    }
+
+    public static List<String> extractNamesColors(List<String> stringList) {
+        CreateNamesHexListFromFileTableNamesHex createNamesHexListFromFileTableNamesHex = new CreateNamesHexListFromFileTableNamesHex();
+        java.util.List<NamesHexTable> namesHexTableList;
+        namesHexTableList = createNamesHexListFromFileTableNamesHex.ToJestProba();
+
+        List<String> extractColorsList = new ArrayList<String>();
+        for (NamesHexTable aNamesHexTableList : namesHexTableList) {
+            String pattern = aNamesHexTableList.getColorName();
+            Pattern p = Pattern.compile(pattern);
+            for (String aStringList : stringList) {
+                Matcher m = p.matcher(aStringList);
+                while (m.find()) {
+                    String results = m.toString();
+                    extractColorsList.add(results);
+                }
+            }
+        }
         return extractColorsList;
     }
 
