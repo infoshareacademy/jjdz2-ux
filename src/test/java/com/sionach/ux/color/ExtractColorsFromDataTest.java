694c6fb7dd6bb14dd96a36c725a2d98cb3fecce2
ablazejewska <allicespring@gmail.com>
2016-10-08 09:50:55 +0200

    poprawiony zapis rgba w tescie

--- src/test/java/com/sionach/ux/color/ExtractColorsFromDataTest.java
+++ src/test/java/com/sionach/ux/color/ExtractColorsFromDataTest.java
@@ -17,9 +17,9 @@ public class ExtractColorsFromDataTest {
         testStringList.add("background-color: #23d4fa;");
         testStringList.add("background-color: #2312aa;");
         testStringList.add("background-color: #b3d1fa;");
-        testStringList.add("background-color: rgba(255,255,255);");
-        testStringList.add("background-color: rgba(255,232,123);");
-        testStringList.add("background-color: rgba(234,212,134);");
+        testStringList.add("background-color: rgba(255,255,255,1);");
+        testStringList.add("background-color: rgba(255,232,123,2);");
+        testStringList.add("background-color: rgba(234,212,134,0);");
         testStringList.add("background-color: rgb(1,255,255);");
         testStringList.add("background-color: rgb(12,232,123);");
         testStringList.add("background-color: rgb(123,212,134);");
