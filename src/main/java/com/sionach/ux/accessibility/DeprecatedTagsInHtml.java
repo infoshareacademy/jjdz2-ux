90461af3d55ac3c879f5bc9b67ea4e7924fcf77b
drzalek <drzalek83@gmail.com>
2016-10-17 17:10:09 +0200

    Dodanie warunku sprawdzajacego czy dokument jest html5

--- src/main/java/com/sionach/ux/accessibility/DeprecatedTagsInHtml.java
+++ src/main/java/com/sionach/ux/accessibility/DeprecatedTagsInHtml.java
@@ -3,11 +3,15 @@ package com.sionach.ux.accessibility;
 import com.sionach.ux.filemanagment.ReadFiles;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by webownia on 10.10.16.
@@ -16,17 +20,27 @@ public class DeprecatedTagsInHtml {
 
     public List<String> findDeprecatedTags(String htmlCode) throws FileNotFoundException {
         List<String> deprecatedTags = new ArrayList<>();
-        Document doc = Jsoup.parse(htmlCode);
-
-        DeprecatedTagsList deprecatedHtmlTags = new DeprecatedTagsList();
-        List<String> deprecatedTagsList = deprecatedHtmlTags.createDeprecatedTagsList("src/main/resources/deprecatedTags.txt");
-        for(String item:deprecatedTagsList) {
-            Elements elements = doc.select(item);
-            if(elements.size()>0){
-                deprecatedTags.add("Tag: <"+item+"> nie jest używany w HTML5");
+        Pattern pattern = Pattern.compile("(?i)<!DOCTYPE html>");
+        Matcher m = pattern.matcher(htmlCode);
+        if(m.find()){
+            Document doc = Jsoup.parse(htmlCode);
+            DeprecatedTagsList deprecatedHtmlTags = new DeprecatedTagsList();
+            List<String> deprecatedTagsList = deprecatedHtmlTags.createDeprecatedTagsList("src/main/resources/deprecatedTags.txt");
+            for(String item:deprecatedTagsList) {
+                Elements elements = doc.select(item);
+                if(elements.size()>0){
+                    deprecatedTags.add("Tag: <"+item+"> nie jest używany w HTML5");
+                }
             }
+            if(deprecatedTags.size()==0){
+                deprecatedTags.add("Dokument nie zawiera zdeprecjonowanych elementów");
+            }
+        }else{
+            deprecatedTags.add("Przesłany dokument nie jest zadeklarowany jako dokument HTML5");
+
         }
         return  deprecatedTags;
     }
+
 
 }
