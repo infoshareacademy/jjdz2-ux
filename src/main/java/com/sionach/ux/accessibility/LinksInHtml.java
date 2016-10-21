a0ccdaa805f599914ee270a1dda6d41309596b5b
Paweł <drzalek83@gmail.com>
2016-10-21 13:03:10 +0200

    odrzucenie linkow zaczynajacych sie od #

--- src/main/java/com/sionach/ux/accessibility/LinksInHtml.java
+++ src/main/java/com/sionach/ux/accessibility/LinksInHtml.java
@@ -3,7 +3,11 @@ package com.sionach.ux.accessibility;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by drzal on 21.10.2016.
@@ -11,18 +15,23 @@ import org.jsoup.select.Elements;
 public class LinksInHtml {
 
     public String checkNumberOfLinks(String htmlCode){
-        String linksInfo="";
+        String linksInfo;
         Document doc = Jsoup.parse(htmlCode);
-        Elements hrefs = doc.select("href");
-        int numberOfHrefs = hrefs.size();
+        Elements hrefs = doc.select("a");
+        List<String> linkList = new ArrayList<>();
 
-        if(numberOfHrefs>100){
-            linksInfo = "Liczba linków na Twojej stronie wynosi "+numberOfHrefs+" i jest ich zbyt dużo. Zalecana maksymalna liczba linków na stronie to 100";
-        }else{
-            linksInfo = "Znaleziono "+numberOfHrefs+" linków na Twojej stronie. Liczba linków jest mniejsza niż 100, więc jest prawidłowa";
+        for(Element item:hrefs){
+            linkList.add(item.attr("href"));
         }
+        long numberOfLinks = linkList.stream()
+                .filter(line -> line.matches("(?i)^[^#]{1}.*$"))
+                .count();
 
-
+        if(numberOfLinks>100){
+            linksInfo = "Liczba linków na Twojej stronie wynosi "+numberOfLinks+" i jest ich zbyt dużo. Zalecana maksymalna liczba linków na stronie to 100";
+        }else{
+            linksInfo = "Znaleziono "+numberOfLinks+" linków na Twojej stronie. Liczba linków jest mniejsza niż 100, więc jest prawidłowa";
+        }
         return linksInfo;
     }
 
