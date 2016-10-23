2423ebb24c6c2d4a8be38546dedf4d3455e6b19d
Paweł Orszt <pawel.orszt@gmail.com>
2016-10-23 11:10:52 +0200

    poprawa importów dodanie deprecjonowanych tagów dodanie badania obrazków dodanie badania linków

--- src/main/java/com/sionach/ux/console/Console.java
+++ src/main/java/com/sionach/ux/console/Console.java
@@ -1,23 +1,18 @@
 package com.sionach.ux.console;
 
-import com.sionach.ux.accessibility.HeadlinesInHtml;
-import com.sionach.ux.accessibility.SettingsInHeadElement;
+import com.sionach.ux.accessibility.*;
 import com.sionach.ux.color.ExtractColorsFromData;
 import com.sionach.ux.csslists.CssListFromHtml;
 import com.sionach.ux.filemanagment.ReadFiles;
 import com.sionach.ux.keyWords.*;
 import com.sionach.ux.routing.LinkMenagement;
-import com.sionach.ux.siteAvailability.CaseOfHtml5;
-import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Console {
@@ -113,12 +108,29 @@ public class Console {
                     case 4:
                         CaseOfHtml5 caseOfHtml5 = new CaseOfHtml5();
                         SettingsInHeadElement settingsInHeadElement = new SettingsInHeadElement();
-                        List<String> settingsInHeadRaport = settingsInHeadElement.checkHeadSettings(htmlInString);
                         HeadlinesInHtml headlinesInHtml = new HeadlinesInHtml();
+                        DeprecatedTagsInHtml deprecatedTagsInHtml = new DeprecatedTagsInHtml();
+                        AltAndTitleInImage altAndTitleInImage = new AltAndTitleInImage();
+                        LinksInHtml linksInHtml = new LinksInHtml();
 
+                        List<String> altAndTitleInImageRaport = altAndTitleInImage.ImageAltTitle(htmlInString);
+                        List<String> settingsInHeadRaport = settingsInHeadElement.checkHeadSettings(htmlInString);
+                        List<String> deprecatedTagsRaport = new ArrayList<>();
+                        try {
+                             deprecatedTagsRaport = deprecatedTagsInHtml.findDeprecatedTags(htmlInString);
+                        } catch (FileNotFoundException e) {
+                            System.out.println("Nie znaleziono pliku z deprecjonowanymi elementami\n" );
+                            e.printStackTrace();
+                        }
 
 
                         caseOfHtml5.CheckIfHtml5(htmlInString);
+                        for(String item : deprecatedTagsRaport){
+                            System.out.println(item);
+                        }
+
+
+
                         System.out.println("\nRaport sekcji HEAD");
                         for(String item: settingsInHeadRaport){
                             System.out.println(item);
@@ -128,6 +140,16 @@ public class Console {
 
                         System.out.println(headlinesInHtml.checkHeadlinesInHtml(htmlInString));
                         System.out.println(headlinesInHtml.checkNoOccurrancesH1(htmlInString));
+
+                        System.out.println("\n");
+
+                        for(String item : altAndTitleInImageRaport){
+                            System.out.println(item);
+                        }
+
+                        System.out.println("\n");
+
+                        System.out.println(linksInHtml.checkNumberOfLinks(htmlInString));
 
                         System.out.println("\n");
                         break;
