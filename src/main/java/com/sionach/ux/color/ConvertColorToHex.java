1f8dcccbe6746142cf59147c8acc39f2626c0e53
ablazejewska <allicespring@gmail.com>
2016-10-23 09:27:17 +0200

    Działająca metoda wybierająca metodę konwersji koloru na podstawie formatu

--- src/main/java/com/sionach/ux/color/ConvertColorToHex.java
+++ src/main/java/com/sionach/ux/color/ConvertColorToHex.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class ConvertColorToHex {
     private String colorHex;  // format #rrggbb
 
-    ConvertColorToHex(){
+    ConvertColorToHex() {
 
         this.colorHex = "#000000";
     }
@@ -17,6 +17,7 @@ public class ConvertColorToHex {
 
         this.colorHex = colorHex;
     }
+
     public String getColorHex() {
 
         return this.colorHex;
@@ -30,64 +31,65 @@ public class ConvertColorToHex {
                 shortHexToHex(stringColor);
             }
         } else if (stringColor.matches("(?i)rgba\\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0-1]{0,3}\\)")) {
-            stringColor = stringColor.replaceAll("\\("," ");
-            stringColor = stringColor.replaceAll("\\)"," ");
-            String[] split = stringColor.split(",", 3);
-            int r = Integer.parseInt(split[0]);
-            int g = Integer.parseInt(split[1]);
-            int b = Integer.parseInt(split[2]);
-            int a = Integer.parseInt(split[3]);
-            rgbaToHex(r,g,b,a);
+            stringColor = stringColor.replaceAll("\\(", " ");
+            stringColor = stringColor.replaceAll("\\)", " ");
+            stringColor = stringColor.replace("rgba"," ");
+            stringColor = stringColor.trim();
+            String[] split = stringColor.split(",", 4);
+            int r = Integer.valueOf(split[0]);
+            int g = Integer.valueOf(split[1]);
+            int b = Integer.valueOf(split[2]);
+            int a = Integer.valueOf(split[3]);
+            rgbaToHex(r, g, b, a);
 
-        }
-        else if (stringColor.matches("(?i)rgb\\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\\)")) {
-            stringColor = stringColor.replaceAll("\\("," ");
-            stringColor = stringColor.replaceAll("\\)"," ");
+        } else if (stringColor.matches("(?i)rgb\\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\\)")) {
+            stringColor = stringColor.replaceAll("\\(", " ");
+            stringColor = stringColor.replaceAll("\\)", " ");
+            stringColor = stringColor.replace("rgb"," ");
+            stringColor = stringColor.trim();
             String[] split = stringColor.split(",", 3);
-            int r = Integer.parseInt(split[0]);
-            int g = Integer.parseInt(split[1]);
-            int b = Integer.parseInt(split[2]);
-            rgbToHex(r,g,b);
-        }
-        else if(stringColor.matches("(?i)[a-z]")){
+            int r = Integer.valueOf(split[0]);
+            int g = Integer.valueOf(split[1]);
+            int b = Integer.valueOf(split[2]);
+            rgbToHex(r, g, b);
+
+        } else {
             nameToHex(stringColor);
-        } else{
-            throw new IllegalArgumentException();    //Pawel, obsłuz wyjatek w Consoli
         }
     }
 
-    public void rgbToHex(int r, int g, int b){
-        Color color = new Color(r,g,b);
-        String hex = Integer.toHexString(color.getRGB()&0xffffff);
-        while (hex.length() < 6){
-            hex = "0" + hex;
-        }
-        this.colorHex = "#" + hex;
-    }
-
-    public void rgbaToHex(int r, int g, int b, int a){
-        Color color = new Color(r,g,b,a);
-        String hex = Integer.toHexString(color.getRGB()&0xffffff);
+    public void rgbToHex(int r, int g, int b) {
+        Color color = new Color(r, g, b);
+        String hex = Integer.toHexString(color.getRGB() & 0xffffff);
         while (hex.length() < 6) {
             hex = "0" + hex;
         }
         this.colorHex = "#" + hex;
     }
 
-    public void shortHexToHex(String shortHex){
-        char[] charArray = new char[3];
-        shortHex.getChars(0,3,charArray,0);
-        String rhex = Character.toString(charArray[0]);
-        String ghex = Character.toString(charArray[1]);
-        String bhex = Character.toString(charArray[2]);
+    public void rgbaToHex(int r, int g, int b, int a) {
+        Color color = new Color(r, g, b, a);
+        String hex = Integer.toHexString(color.getRGB() & 0xffffff);
+        while (hex.length() < 6) {
+            hex = "0" + hex;
+        }
+        this.colorHex = "#" + hex;
+    }
+
+    public void shortHexToHex(String shortHex) {
+        char[] charArray = new char[4];
+        shortHex.getChars(0, 4, charArray, 0);
+        String rhex = Character.toString(charArray[1]);
+        String ghex = Character.toString(charArray[2]);
+        String bhex = Character.toString(charArray[3]);
         String hex = rhex + rhex + ghex + ghex + bhex + bhex;
         this.colorHex = "#" + hex;
     }
 
-    public void nameToHex(String name){
+    public void nameToHex(String name) {
         CreateNamesHexListFromFileTableNamesHex createNamesHexListFromFileTableNamesHex = new CreateNamesHexListFromFileTableNamesHex();
         java.util.List<NamesHexTable> namesHexTableList;
-        namesHexTableList =  createNamesHexListFromFileTableNamesHex.FileTolist();
+        namesHexTableList = createNamesHexListFromFileTableNamesHex.FileTolist();
 
         //porównuje name z pierwszym polem namesHexTableList, jeśli równość zachodzi, przypisuje zmiennej hex drugie pole
         String hex = "brak szukanego koloru";
