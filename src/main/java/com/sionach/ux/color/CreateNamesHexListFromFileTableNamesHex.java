b25b95141baa92d148a8cfcf7f00b3233d866178
ablazejewska <allicespring@gmail.com>
2016-11-02 15:47:21 +0100

    naprawiony bład dotyczacy tworzenia listy kolorow z plikow

--- src/main/java/com/sionach/ux/color/CreateNamesHexListFromFileTableNamesHex.java
+++ src/main/java/com/sionach/ux/color/CreateNamesHexListFromFileTableNamesHex.java
@@ -3,16 +3,19 @@ package com.sionach.ux.color;
 import com.sionach.ux.filemanagment.ReadFiles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreateNamesHexListFromFileTableNamesHex {
 
     private static final Logger LOGGER = LogManager.getLogger(CreateNamesHexListFromFileTableNamesHex.class);
 
-    private List<NamesHexTable> namesHexTableList;
+
 
     public List<NamesHexTable> fileToStringList() {
         LOGGER.debug("Attempt to read the file");
+        List<NamesHexTable> namesHexTableList;
         ReadFiles readFile = new ReadFiles();
         List<String> stringList;
         stringList = readFile.readFileToList("tableNamesHex.txt");
@@ -25,12 +28,13 @@ public class CreateNamesHexListFromFileTableNamesHex {
 
     private List<NamesHexTable> StringListToNamesHexTableList(List<String> stringList) {
         LOGGER.debug("Converts list of strings into list of NamesHexTable objects");
+        List<NamesHexTable> namesHexTableLists = new ArrayList<>();
         NamesHexTable namesHexTable = new NamesHexTable();
         for (String aStringList : stringList) {
             namesHexTable.splitLineBySpace(aStringList);
-            namesHexTableList.add(new NamesHexTable(namesHexTable.getColorName(), namesHexTable.getColorHex()));
+            namesHexTableLists.add(new NamesHexTable(namesHexTable.getColorName(), namesHexTable.getColorHex()));
             LOGGER.debug("One string line converted into NamesHexTable object");
         }
-        return namesHexTableList;
+        return namesHexTableLists;
     }
 }
