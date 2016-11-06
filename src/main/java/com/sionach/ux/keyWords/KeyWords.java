bad6d0a07eb4290a6345ab4db376cb9d757dc67c
Paweł Orszt <pawel.orszt@gmail.com>
2016-11-06 12:11:03 +0100

    poprawa importów

--- src/main/java/com/sionach/ux/keyWords/KeyWords.java
+++ src/main/java/com/sionach/ux/keyWords/KeyWords.java
@@ -6,7 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import javax.ejb.Stateless;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Stateless
