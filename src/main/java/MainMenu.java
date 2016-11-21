3583bb9a2c37374a978264b61ac6f0b5e1286c88
ablazejewska <allicespring@gmail.com>
2016-11-21 18:26:26 +0100

    Login to facebook --> klasy testowe

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,13 +1,13 @@
-package com.javapapers.java.social.facebook;
-
-import java.io.IOException;
-import java.util.Map;
+import com.sionach.ux.facebook.FBConnection;
+import com.sionach.ux.facebook.FBGraph;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 public class MainMenu extends HttpServlet {
 
