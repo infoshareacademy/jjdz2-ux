1b81311e8b0c05331b89fc223f1417204bcdc712
Paweł Orszt <pawel.orszt@gmail.com>
2016-11-22 22:49:01 +0100

    przetwarzanie odpowiedzi z formularza na odpowiedzi z pliku

--- src/main/java/FormServlet.java
+++ src/main/java/FormServlet.java
@@ -1,3 +1,6 @@
+import com.sionach.ux.form.PageValidationForm;
+
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -5,18 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(urlPatterns = "/form")
 public class FormServlet extends HttpServlet {
 
+    @EJB
+    PageValidationForm pageValidationForm;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Map<String, String[]> parameterMap = req.getParameterMap();
 
-        System.out.println("\n##Jestem tutaj##\n");
-        System.out.println(parameterMap);
-        req.setAttribute("answers", parameterMap);
+        Map<String, String> answersMap;
+
+        answersMap = pageValidationForm.generateAnswers(parameterMap);
+
+
+        req.setAttribute("answers", answersMap);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/form.jsp");
         System.out.println("wysyłam");
         dispatcher.forward(req, resp);
