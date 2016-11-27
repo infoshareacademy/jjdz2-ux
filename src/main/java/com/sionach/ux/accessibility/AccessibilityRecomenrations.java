6091f1d43a4c4a2d035e8e1b38a4c240c56edee5
drzalek <drzalek83@gmail.com>
2016-11-27 20:47:41 +0100

    dodanie klasy do wyswietlania rekomendacji dla sekcji head

---
+++ src/main/java/com/sionach/ux/accessibility/AccessibilityRecomenrations.java
@@ -0,0 +1,19 @@
+package com.sionach.ux.accessibility;
+
+import com.sun.org.apache.xerces.internal.impl.xs.util.LSInputListImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by drzalek on 27.11.16.
+ */
+public class AccessibilityRecomenrations {
+
+    public List<Object> checkHeadParameters(){
+        List<Object> recomendationsList = new ArrayList<>();
+
+        return recomendationsList;
+    }
+
+}
