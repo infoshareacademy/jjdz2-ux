c2e770f804d9af05c11ade512c82cfeafc590b2f
AlliceSpring <allicespring21@gmail.com>
2016-12-10 19:55:31 +0100

    logowanie do fb

--- src/main/java/com/sionach/ux/facebook/FacebookCallback.java
+++ src/main/java/com/sionach/ux/facebook/FacebookCallback.java
@@ -4,9 +4,11 @@ import com.github.scribejava.apis.FacebookApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.oauth.OAuth20Service;
 import com.github.scribejava.core.oauth.OAuthService;
 
 import javax.json.JsonObject;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,8 +23,19 @@ import java.util.Enumeration;
 
 @WebServlet(urlPatterns = "/callback")
 public class FacebookCallback extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        OAuth20Service service = new ServiceBuilder()
+                .apiKey("339512966407211")
+                .apiSecret("f3f144e2ef310fbbf507f8ed8dac0e7c")
+                .callback("http://localhost:8080/sionach-ux/callback")
+                .build(FacebookApi.instance());
+
+        String code = req.getParameter("code");
+        System.out.println(code);
 
 
     }
