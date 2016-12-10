5d02aee6d38630c5dffcc0b73c2d4e7ba5189181
AlliceSpring <allicespring21@gmail.com>
2016-12-10 20:10:43 +0100

    logowanie do fb

--- src/main/java/com/sionach/ux/facebook/FacebookCallback.java
+++ src/main/java/com/sionach/ux/facebook/FacebookCallback.java
@@ -2,20 +2,14 @@ package com.sionach.ux.facebook;
 
 import com.github.scribejava.apis.FacebookApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
-import com.github.scribejava.core.model.OAuthRequest;
-import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.oauth.OAuth20Service;
-import com.github.scribejava.core.oauth.OAuthService;
-
-import javax.json.JsonObject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
+
 
 /**
  * Created by ablazejewska on 10.12.16.
