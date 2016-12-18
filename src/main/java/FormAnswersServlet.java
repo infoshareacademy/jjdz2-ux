8f545fe0f7393655cc9c635a961abd926670b46a
Paweł Orszt <pawel.orszt@gmail.com>
2016-12-18 19:09:08 +0100

    dodanie przeniesienia przy wywałaniu przez get

--- src/main/java/FormAnswersServlet.java
+++ src/main/java/FormAnswersServlet.java
@@ -1,4 +1,6 @@
 import com.sionach.ux.form.MatchAnswers;
+
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,15 +14,23 @@ import java.util.Map;
 @WebServlet(urlPatterns = "/formAnswers")
 public class FormAnswersServlet extends HttpServlet {
 
+    @EJB
+    MatchAnswers matchAnswers;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Map<String, String[]> parameterMap = req.getParameterMap();
-        MatchAnswers matchAnswers = new MatchAnswers();
         List<String> answers = matchAnswers.getAnswers(parameterMap);
 
         req.setAttribute("answers", answers);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/formAnswers.jsp");
         dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect(req.getContextPath() + "/form");
     }
 }
