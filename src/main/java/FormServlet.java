9343a7ce2f612525a884ccd4d579338d14dc3281
Paweł Orszt <pawel.orszt@gmail.com>
2016-12-18 19:09:35 +0100

    refaktor

--- src/main/java/FormServlet.java
+++ src/main/java/FormServlet.java
@@ -18,7 +18,7 @@ public class FormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.setAttribute("questions", qJsonReader.readJsonFile());
+        req.setAttribute("questions", qJsonReader.getQuestionsForm());
         RequestDispatcher dispatcher = req.getRequestDispatcher("/form.jsp");
         dispatcher.forward(req, resp);
     }
