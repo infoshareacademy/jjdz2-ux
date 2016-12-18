55bcd1113d985e582a6c183d4f80ac0ec5379d2b
Paweł Orszt <pawel.orszt@gmail.com>
2016-12-18 19:09:58 +0100

    zmiana na stateless

--- src/main/java/com/sionach/ux/form/MatchAnswers.java
+++ src/main/java/com/sionach/ux/form/MatchAnswers.java
@@ -1,24 +1,24 @@
 package com.sionach.ux.form;
 
-import java.io.IOException;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+@Stateless
 public class MatchAnswers {
+
+    @EJB
+    QuestionsJsonReader qJsonReader;
 
     public List<String> getAnswers(Map<String, String[]> parameters) {
 
-        QuestionsJsonReader qJsonReader = new QuestionsJsonReader();
-        QuestionsForm questions = new QuestionsForm();
-        try {
-            questions = qJsonReader.readJsonFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         List<String> answers = new LinkedList<>();
 
-        for (Question item : questions.getQuestionsAndAnswers()) {
+        List<Question> questionsForm = qJsonReader.getQuestionsForm().getQuestionsAndAnswers();
+
+        for (Question item : questionsForm) {
             answers.add(item.getTitle());
             for (Answer answer : item.getAnswers()) {
                 if (parameters.get(String.valueOf(item.getKey()))[0].equals(String.valueOf(answer.getOption()))) {
