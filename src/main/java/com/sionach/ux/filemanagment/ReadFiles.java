e10de17b788bd7f44c4c0e9c5fc627ef4edb65c0
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-03 19:36:17 +0100

    czyszczenie kodu

--- src/main/java/com/sionach/ux/filemanagment/ReadFiles.java
+++ src/main/java/com/sionach/ux/filemanagment/ReadFiles.java
@@ -9,9 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Stateless
 public class ReadFiles {
