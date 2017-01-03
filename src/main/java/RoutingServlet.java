8bba258e184d8d2a755541255d7fa1b6d85be14c
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-03 21:16:48 +0100

    szkielet do wyświetlania routingu

--- src/main/java/RoutingServlet.java
+++ src/main/java/RoutingServlet.java
@@ -1,3 +1,11 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 //import com.sionach.ux.filemanagment.ReadFiles;
 //import com.sionach.ux.routing.LinkManagement;
 //
@@ -48,3 +56,20 @@
 //    }
 //
 //}
+
+@WebServlet(urlPatterns = "/routing")
+public class RoutingServlet extends HttpServlet{
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/routing.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/routing.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/routing.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<jsp:include page="header.jsp"/>
+
+<main class="container">
+
+
+</main>
+
+
+<jsp:include page="footer.jsp"/>
