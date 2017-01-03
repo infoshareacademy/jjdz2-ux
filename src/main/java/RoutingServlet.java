5e7784951a8787b357e59f7ebf990c4800d4a66f
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-03 22:25:34 +0100

    obsługa generowania linków i przesyłanie ich na stronę z raportem

--- src/main/java/RoutingServlet.java
+++ src/main/java/RoutingServlet.java
@@ -1,3 +1,6 @@
+import com.sionach.ux.routing.LinkManagement;
+
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -5,9 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/routing")
 public class RoutingServlet extends HttpServlet{
+
+    @EJB
+    LinkManagement linkManagement;
 
 
     @Override
@@ -19,6 +26,12 @@ public class RoutingServlet extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String link = req.getParameter("domainurl");
+
+        List<String> innerLinks = linkManagement.getInnerLinks(linkManagement.parseLinksFromHtml(linkManagement.getPageAsDocument(link)), link);
+        List<String> outerLinks = linkManagement.getOuterLinks(linkManagement.parseLinksFromHtml(linkManagement.getPageAsDocument(link)), link);
+
+        req.setAttribute("innerLinks", innerLinks);
+        req.setAttribute("outerLinks", outerLinks);
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/routing.jsp");
         dispatcher.forward(req, resp);
