36636f5692acbcce3b73e7da6638c2f5e83acddc
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-04 23:52:27 +0100

    dodanie id pytania do pytania

--- src/main/java/com/sionach/ux/form/MatchAnswers.java
+++ src/main/java/com/sionach/ux/form/MatchAnswers.java
@@ -19,7 +19,7 @@ public class MatchAnswers {
         List<Question> questionsForm = qJsonReader.getQuestionsForm().getQuestionsAndAnswers();
 
         for (Question item : questionsForm) {
-            answers.add(item.getTitle());
+            answers.add(item.getKey() + item.getTitle());
             for (Answer answer : item.getAnswers()) {
                 if (parameters.get(String.valueOf(item.getKey()))[0].equals(String.valueOf(answer.getOption()))) {
                     answers.add(String.valueOf(answer.getOption()) + answer.getAnswer());
