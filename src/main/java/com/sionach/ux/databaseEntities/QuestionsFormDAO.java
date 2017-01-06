da372c3d1741ec3bf43ca4969571484530442f21
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-06 19:59:03 +0100

    dodanie odczytu z bazy dodanie logera

--- src/main/java/com/sionach/ux/databaseEntities/QuestionsFormDAO.java
+++ src/main/java/com/sionach/ux/databaseEntities/QuestionsFormDAO.java
@@ -1,5 +1,8 @@
 package com.sionach.ux.databaseEntities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,25 +11,34 @@ import java.util.List;
 @Stateless
 public class QuestionsFormDAO {
 
+    private static final Logger LOGGER = LogManager.getLogger(QuestionsForm.class);
     @PersistenceContext
     EntityManager entityManager;
 
     public void saveForm(List<String> formQuestionsAndAnswers) {
+
         for (String item : formQuestionsAndAnswers) {
             QuestionsForm questionsForm = new QuestionsForm();
             if (item.contains("?")) {
-                System.out.println("PYTANIE###    " + item.substring(0, 1));
+                LOGGER.debug("PYTANIE###    " + item.substring(0, 1));
                 questionsForm.setQuestion_id(Integer.parseInt(item.substring(0, 1)));
             } else {
-                System.out.println("ODPOWIEDŹ#    " + item.substring(0, 1));
+                LOGGER.debug("ODPOWIEDŹ#    " + item.substring(0, 1));
                 questionsForm.setAnswer(Integer.parseInt(item.substring(0, 1)));
             }
             entityManager.persist(questionsForm);
         }
+        LOGGER.info("Zapis do bazy danych");
     }
 
     public void readForm() {
-        //?????
-        List items =  entityManager.createQuery("SELECT d FROM QuestionsForm d").getResultList();
+
+        List<QuestionsForm> items =  entityManager.createQuery("SELECT d FROM QuestionsForm d").getResultList();
+
+        for (QuestionsForm item : items){
+
+            System.out.println(item.toString());
+        }
+        LOGGER.info("Odczyt z bazy danych");
     }
 }
