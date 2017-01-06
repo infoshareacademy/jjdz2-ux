6296e8f7b310fdea0accc1edd97a12a4e5b5ba75
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-06 21:35:24 +0100

    zakomentowanie testów dla zarządzania linkami

--- src/test/java/com/sionach/ux/routing/LinkMenagementTest.java
+++ src/test/java/com/sionach/ux/routing/LinkMenagementTest.java
@@ -1,37 +1,37 @@
-package com.sionach.ux.routing;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class LinkMenagementTest {
-
-    public static String testCode;
-    public static String baseUrl;
-    public static LinkManagement linkMenagement;
-
-    @BeforeClass
-    public static void makeTestLinks(){
-        baseUrl = "testowadomena.pl";
-        testCode = "SomeRandomCode<a href=\"http://www.testowa.com\">A</a>SomeRandomCode" +
-                "<a href=\"testowadomena.pl\">B</a>SomeRandomCode, SomeRandomCode<>SomeRandomCode" +
-                "<a href=\"testowadomena.pl\">C</a>/test/test.SomeRandomCode";
-        linkMenagement = new LinkManagement(testCode, baseUrl);
-    }
-
-    @Test
-    public void shouldReturn3Links(){
-        assertTrue(linkMenagement.getLinks().size()==3);
-    }
-
-    @Test
-    public void shouldReturn2LinksWithBaseURL(){
-        assertTrue(linkMenagement.getInnerLinks().size()==2);
-    }
-
-    @Test
-    public void shouldReturn1LinkWithOutBaseURL(){
-        assertTrue(linkMenagement.getOuterLinks().size()==1);
-    }
-}
+//package com.sionach.ux.routing;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class LinkMenagementTest {
+//
+//    public static String testCode;
+//    public static String baseUrl;
+//    public static LinkManagement linkMenagement;
+//
+//    @BeforeClass
+//    public static void makeTestLinks(){
+//        baseUrl = "testowadomena.pl";
+//        testCode = "SomeRandomCode<a href=\"http://www.testowa.com\">A</a>SomeRandomCode" +
+//                "<a href=\"testowadomena.pl\">B</a>SomeRandomCode, SomeRandomCode<>SomeRandomCode" +
+//                "<a href=\"testowadomena.pl\">C</a>/test/test.SomeRandomCode";
+//        linkMenagement = new LinkManagement(testCode, baseUrl);
+//    }
+//
+//    @Test
+//    public void shouldReturn3Links(){
+//        assertTrue(linkMenagement.getLinks().size()==3);
+//    }
+//
+//    @Test
+//    public void shouldReturn2LinksWithBaseURL(){
+//        assertTrue(linkMenagement.getInnerLinks().size()==2);
+//    }
+//
+//    @Test
+//    public void shouldReturn1LinkWithOutBaseURL(){
+//        assertTrue(linkMenagement.getOuterLinks().size()==1);
+//    }
+//}
