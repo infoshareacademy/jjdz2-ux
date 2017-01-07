4c22fe977f235b719e001f8827d0da9410770d1d
ablazejewska <allicespring@gmail.com>
2017-01-07 10:37:38 +0100

    Kolory z Url

--- src/main/java/ColorsServlet.java
+++ src/main/java/ColorsServlet.java
@@ -1,14 +1,11 @@
 import com.sionach.ux.color.ClipColors;
 import com.sionach.ux.color.CssListFromHtml;
-import com.sionach.ux.databaseEntities.Colors;
 import com.sionach.ux.databaseEntities.ColorsDAO;
 import com.sionach.ux.filemanagment.ReadFiles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.ejb.EJB;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 /**
  * Created by allic on 02/11/2016.
@@ -50,15 +46,9 @@ public class ColorsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        String choose = req.getParameter("choose");
-        cssFile.setDefaultPatch("target/classes/" + choose + "/style.css");
-        htmlFile.setDefaultPatch("target/classes/" + choose + "/");
+        String urlPattern = req.getParameter("setHTMLpathColors");
 
-        String htmlInString = htmlFile.readFileToString("index.html");
-
-
-
-        Set<String> distinctHex = clipColors.ClipColorsFromData(htmlInString,cssFile);
+        Set<String> distinctHex = clipColors.ClipColorsFromData(urlPattern);
         LOGGER.info("przekazujemy Color Servelt {}",clipColors );
 
         colorsDAO.save(distinctHex);
