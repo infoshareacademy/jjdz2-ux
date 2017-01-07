d04d946d3442db1978ddffd59a8f1b6382435e7d
ablazejewska <allicespring@gmail.com>
2017-01-07 11:54:36 +0100

    aplikacja fb

--- src/main/java/com/sionach/ux/facebook/FacebookLogin.java
+++ src/main/java/com/sionach/ux/facebook/FacebookLogin.java
@@ -4,12 +4,9 @@ package com.sionach.ux.facebook;
  * Created by ablazejewska on 10.12.16.
  */
 import com.github.scribejava.apis.FacebookApi;
-
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.model.Token;
 import com.github.scribejava.core.oauth.OAuth20Service;
-import com.github.scribejava.core.oauth.OAuthService;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -32,8 +29,8 @@ public class FacebookLogin extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         OAuth20Service service = new ServiceBuilder()
-                .apiKey("339512966407211")
-                .apiSecret("f3f144e2ef310fbbf507f8ed8dac0e7c")
+                .apiKey("339512169740624")
+                .apiSecret("e720048a5e62f58b44bf6b36971fbcfb")
                 .callback("http://localhost:8080/sionach-ux/callback")
                 .build(FacebookApi.instance());
 
