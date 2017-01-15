9ab6079740c2d695b107a2f5e622972a9f4170ae
Paweł Orszt <pawel.orszt@gmail.com>
2017-01-15 20:07:36 +0100

    dodanie sortowania listy po liczbie wystąpień koloru

--- src/main/java/com/sionach/ux/reports/ColorReport.java
+++ src/main/java/com/sionach/ux/reports/ColorReport.java
@@ -4,6 +4,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Stateless
 public class ColorReport {
@@ -17,6 +18,9 @@ public class ColorReport {
                 "SELECT NEW com.sionach.ux.reports.Color(c.colors, count(c) as count)" +
                         " FROM Colors c GROUP BY c.colors").getResultList();
 
-        return colors;
+        return colors.stream().
+                sorted((color1, color2) -> Long.compare(color2.getCount(), color1.getCount())).
+                collect(Collectors.toList());
     }
 }
+
