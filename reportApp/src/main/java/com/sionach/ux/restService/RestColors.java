6348f145e05494079a8aa95625ab2ab9b8b6d9e1
AlliceSpring <allicespring21@gmail.com>
2017-02-19 20:23:56 +0100

    Przeniesienie rapotu colorow do modulu raportowego

---
+++ reportApp/src/main/java/com/sionach/ux/restService/RestColors.java
@@ -0,0 +1,34 @@
+package com.sionach.ux.restService;
+
+
+import com.sionach.ux.colorsReport.Color;
+import com.sionach.ux.colorsReport.ColorReport;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/restcolors")
+public class RestColors {
+
+    @Inject
+    ColorReport colorReport;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getcolors")
+    public List<Color> getColors(){
+        return colorReport.getColorsWithCounter();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    public List<Color> saveColors(){
+        return colorReport.getColorsWithCounter();
+    }
+
+
+}
